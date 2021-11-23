0b9d3f47e0fc36d2163720926b42e5b686b6f6a3
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-11-23 18:22:50 -0300

    update

---
+++ to-do/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/shared/header/header";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes></Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ to-do/src/components/shared/header/header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import img from "../../../img/checklogo.png";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-light bg-light" id="header1">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <img
+            src={img}
+            alt="logo"
+            width="30"
+            height="24"
+            className="d-inline-block align-text-top"
+          />
+          To Do
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
