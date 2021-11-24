053ab4ba09ec16881d36b3514fef3d14387a6e12
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-11-24 02:29:17 -0300

    update

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import Header from "./components/shared/header/header";
+import Home from "./pages/home";
+import Cadastro from "./pages/cadastro";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<Cadastro />} />
+      </Routes>
     </div>
   );
 }
--- to-do/src/components/shared/header/header.js
+++ to-do/src/components/shared/header/header.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import img from "../../../img/checklogo.png";
+import "./header.css";
 
 const Header = () => {
   return (
     <nav className="navbar navbar-light bg-light" id="header1">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand logotodo" to="/">
           <img
             src={img}
             alt="logo"
             width="30"
-            height="24"
+            height="30"
             className="d-inline-block align-text-top"
           />
           To Do
+        </Link>
+        <Link className="navbar-brand flex-end buttonsH" to="/add">
+          New Task
         </Link>
       </div>
     </nav>
