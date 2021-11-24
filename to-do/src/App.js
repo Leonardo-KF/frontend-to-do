9b3379cc291e5823884b007fd37313f102e45499
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-11-24 16:12:07 -0300

    update

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 import Header from "./components/shared/header/header";
+import Footer from "./components/shared/footer/footer";
 import Home from "./pages/home";
 import Cadastro from "./pages/cadastro";
+import View from "./pages/view";
+import Edit from "./pages/edit";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,7 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/add" element={<Cadastro />} />
+        <Route path="/view/:id" element={<View />} />
+        <Route path="/edit/:id" element={<Edit />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
